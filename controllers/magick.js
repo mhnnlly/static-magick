c117f61c4a007d511e65b95a842eddd91f9d319e
Mike Hennelly <michaelthehennelly@gmail.com>
2021-06-11 23:33:38 -0700

    Fetching target content
    Using node-fetch package to fetch files, chose fetch over http for convenient data limiting

--- controllers/magick.js
+++ controllers/magick.js
@@ -1,13 +1,33 @@
+const fetch = require('node-fetch')
+const path = require('path')
 const url = require('url')
 
-exports.magick = (req, res) => {
+const extensions = ['.jpg', '.png', '.jpeg']
+
+async function downloadImg (url) {
+    let response = await fetch(url);
+    let blob = await response.blob();
+    return blob
+}
+
+exports.magick = async (req, res) => {
+    let errRes = (msg) => {
+	res.render('error.ejs', {
+	    message: msg
+	})
+    }
     let queryObj = url.parse(req.url, true).query
     let target = queryObj['target']
     if (!target) {
-	res.render('error.ejs', {
-	    message: 'Missing Target Path'
-	})
+	errRes('Missing Target File')
 	return
     }
-    res.send(queryObj)
+    let targetExt = path.extname(target)
+    if (!targetExt || extensions.indexOf(targetExt) === -1) {
+	errRes('Target Must Have Extension jpg/jpeg/png')
+	return
+    }
+    imgBlob = await downloadImg(target)
+    imgBuff = await imgBlob.arrayBuffer()
+    res.send(Buffer.from(imgBuff))
 }
