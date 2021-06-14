60e0c63f58755fb33436c0c7a6894569b4431eeb
Mike Hennelly <michaelthehennelly@gmail.com>
2021-06-13 20:20:17 -0700

    Got blur working
    Blur works now, whole page responds to changes specified in the form

--- public/js/index.jsx
+++ public/js/index.jsx
@@ -4,24 +4,42 @@ class App extends React.Component {
 
     constructor(props) {
 	super(props)
-	this.state = {date: new Date(),
-		      srcURL: props.srcURL,
+	this.state = {srcURL: props.srcURL,
 		      filterParams: '&negative'}
+	this.handleChange = this.handleChange.bind(this)
+	this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    getParams(state) {
+	const filters = ['blur']
+
+	const filtMap = {
+	    'blur': (state) => {return state.blurX && state.blurY ?
+				'&blur=' + state.blurX + ',' + state.blurY : ''}
+	}
+	let res = ''
+	filters.forEach((val, index, array) => {
+	    res += filtMap[val](state)
+	})
+	return res
     }
 
-    componentDidMount() {
-	this.timerID = setInterval(
-	    () => this.tick(),
-	    1000)
-    }
-
-    componentWillUnmount() {
-	clearInterval(this.timerID)
-    }
-
-    tick() {
+    handleChange(e) {
+	e.preventDefault()
+	const target = event.target
+	const value = target.type === 'checkbox' ? target.checked : target.value
+	const name = target.name
 	this.setState({
-	    date: new Date()
+	   [name]: value
+	})
+	console.log(this.state)
+    }
+
+    handleSubmit(e) {
+	e.preventDefault()
+	this.setState({
+	    srcURL: this.state.src,
+	    filterParams: this.getParams(this.state)
 	})
     }
     
@@ -31,36 +49,55 @@ class App extends React.Component {
 			this.state.srcURL + this.state.filterParams
 	return (
 	    <div class='container'>
+		<br/>
 		<div class='row'>
 		    <h1>Static Magick</h1>
 		</div>
 		<div class='row'>
-		    <div class='col-sm-4'>
-			<p>The Original Static Image</p>
+		    <div class='col-4'>
+			<p>The Original Image</p>
 			<figure class='figure'>
-			    <img src={srcURL} class='figure-img img-fluid rounded'
+			    <img src={srcURL} class='figure-img img-fluid rounded img-thumbnail'
 			    alt=':)'/>
-			    <figcaption class='figure-caption'><a href={filterURL}>{srcURL}
+			    <figcaption class='figure-caption'><a href={srcURL}>{srcURL}
 			    </a></figcaption>
 			</figure>
 		    </div>
-		    <div class='col-sm-4'>
-			<p>The Filtered Result</p>
+		    <div class='col-4 offset-2'>
+			<p>With Filter(s)</p>
 			<figure class='figure'>
 			    <img src={filterURL}
-				 class='figure-img img-fluid rounded' alt=':)'/>
+				 class='figure-img img-fluid rounded img-thumbnail' alt=':)'/>
 			    <figcaption class='figure-caption'><a href={filterURL}>{filterURL}
 			    </a></figcaption>
 			</figure>
 		    </div>
 		</div>
 		<div class='row'>
-		    <form>
-			<label>
-			    Static Image Absolute URL:
-			    <input type='text' name='src'/>
-			</label>
-			<input type='submit' value='Submit'/>
+		    <form class='col-8' onSubmit={this.handleSubmit}>
+			<div class='form-group'>
+			    <label>Static Image Absolute URL:</label>
+			    <input class='form-control' onChange={this.handleChange} type='text'
+				   name='src' placeholder='Ex: https://website.web/image.png'/>
+			    <small class='form-text text-muted'>
+				Images must be less than 30MB and public
+			    </small>
+			</div>
+			<div class='form-group row'>
+			    <label>Blur:</label>
+				<div class='col-3'>
+				    <input class='form-control' onChange={this.handleChange} type='number'
+					   name='blurX' placeholder='Radius'/>
+				</div>
+				<div class='col-3'>
+				    <input class='form-control' onChange={this.handleChange} type='number'
+					   name='blurY' placeholder='Sigma'/>
+				</div>
+			    <small class='form-text text-muted'>
+				Apply Gaussian blur to image with radius and standard deviation
+			    </small>
+			</div>
+			<button type='submit' class='btn btn-primary'>Submit</button>
 		    </form>
 		</div>
 	    </div>
